7d5e91aba76c7f18fbac0b2ba1e58823b922d449
송영현 <dudgus907@naver.com>
2023-06-26 12:58:53 +0900

    sytle : 회원가입 페이지 순서 변경 페이지 2와 페이지3의 순서를 변경했습니다.

--- app/src/main/java/com/example/remak/signUp/AccountSignUp2Fragment.kt
+++ app/src/main/java/com/example/remak/signUp/AccountSignUp2Fragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.remak.BaseFragment
-import com.example.remak.R
 import com.example.remak.databinding.AccountSignup2FragmentBinding
+import com.example.remak.databinding.AccountSignup3FragmentBinding
 
-class AccountSignUp2Fragment : BaseFragment() {
+class AccountSignUp2Fragment : Fragment() {
     private lateinit var binding : AccountSignup2FragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,23 +21,5 @@ class AccountSignUp2Fragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        var passwordValid = false
-        var passwordRepeatValid = false
-
-        binding.root.setOnClickListener {
-            hideKeyboard()
-        }
-
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            findNavController().navigate(R.id.action_accountSignUp2Fragment2_to_accountSignUp1Fragment2)
-        }
-        passwordCheck(binding.passwordEditText, binding.passwordCheckEditText, binding.nextBtn)
     }
-
-    //비밀번호 유효성 검사
-
-
-
-
 }
