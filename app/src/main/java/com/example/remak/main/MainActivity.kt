686292127902aba9c78e40d5bdbefcc4c97086e5
송영현 <dudgus907@naver.com>
2023-06-27 19:51:43 +0900

    Design : 아이콘 추가

--- app/src/main/java/com/example/remak/main/MainActivity.kt
+++ app/src/main/java/com/example/remak/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.remak.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.remak.R
+import com.example.remak.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
--- app/src/main/java/com/example/remak/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/example/remak/signUp/SignUpViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.remak
+package com.example.remak.signUp
 
 import android.app.Application
 import android.view.inputmethod.InputMethodManager
