972fe5e240578d3f42d136f043b3bd99f640ae1d
송영현 <dudgus907@naver.com>
2023-06-27 18:57:22 +0900

    Feat : 메인Activity 및 Fragment파일 추가

--- app/src/main/java/com/example/remak/main/MainActivity.kt
+++ app/src/main/java/com/example/remak/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.remak
+package com.example.remak.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.remak.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/remak/main/MainHomeFragment.kt
@@ -0,0 +1,4 @@
+package com.example.remak.main
+
+class MainHomeFragment {
+}
