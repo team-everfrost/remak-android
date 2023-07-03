5122ebca5e6de48b6d7169b931a158cf0bf2f1fd
송영현 <dudgus907@naver.com>
2023-07-03 18:16:26 +0900

    Design : tag RV 및 item 추가

--- app/src/main/java/com/example/remak/view/main/MainTagFragment.kt
+++ app/src/main/java/com/example/remak/view/main/MainTagFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.remak.BaseFragment
 import com.example.remak.databinding.MainTagFragmentBinding
 
@@ -25,6 +26,14 @@ class MainTagFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setRV()
+    }
+
+    private fun setRV() {
+        val testTagRV = TestTagRVAdapter(arrayOf("test", "test", "test", "test", "test", "test", "test", "test", "test", "test"))
+        binding!!.tagRV.adapter = testTagRV
+        binding!!.tagRV.layoutManager = LinearLayoutManager(requireContext())
     }
 
 
---
+++ app/src/main/java/com/example/remak/view/main/TestTagRVAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.remak.view.main
+
+import android.graphics.Rect
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.remak.R
+
+class TestTagRVAdapter (private val testData : Array<String>) : RecyclerView.Adapter<TestTagRVAdapter.TestTagRVViewHolder>(){
+
+    inner class TestTagRVViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val title = view.findViewById<TextView>(R.id.title)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestTagRVViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.tag_item, parent, false)
+
+        return TestTagRVViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return testData.size
+    }
+
+    override fun onBindViewHolder(holder: TestTagRVViewHolder, position: Int) {
+    }
+
+}
+
+
+
