3df5b4a1baa992ae8e006ff5ecc65df1d1c25221
song-yeonghyun <dudgus907@naver.com>
2023-09-14 23:43:51 +0900

    Design : 내정보 페이지 디자인

--- app/src/main/java/com/example/remak/view/main/MainProfileFragment.kt
+++ app/src/main/java/com/example/remak/view/main/MainProfileFragment.kt
@@ -1,6 +1,5 @@
 package com.example.remak.view.main
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.remak.App
 import com.example.remak.R
-import com.example.remak.UtilityDialog
 import com.example.remak.UtilitySystem
 import com.example.remak.dataStore.TokenRepository
 import com.example.remak.databinding.MainProfileFragmentBinding
-import com.example.remak.view.account.AccountActivity
 
 class MainProfileFragment : Fragment() {
     private lateinit var binding: MainProfileFragmentBinding
@@ -46,20 +43,20 @@ class MainProfileFragment : Fragment() {
             UtilitySystem.hideKeyboard(requireActivity())
         }
 
-        binding.logoutButton.setOnClickListener {
-            UtilityDialog.showWarnDialog(requireContext(), "로그아웃 하시겠습니까?",
-                "",
-                "네",
-                "아니오",
-                confirmClick = {
-                    viewModel.deleteToken()
-                    val intent = Intent(requireContext(), AccountActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    startActivity(intent)
-                }, cancelClick = {
-                    //do nothing
-                })
-        }
+//        binding.logoutButton.setOnClickListener {
+//            UtilityDialog.showWarnDialog(requireContext(), "로그아웃 하시겠습니까?",
+//                "",
+//                "네",
+//                "아니오",
+//                confirmClick = {
+//                    viewModel.deleteToken()
+//                    val intent = Intent(requireContext(), AccountActivity::class.java)
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+//                    startActivity(intent)
+//                }, cancelClick = {
+//                    //do nothing
+//                })
+//        }
     }
 
     //ondestroy
